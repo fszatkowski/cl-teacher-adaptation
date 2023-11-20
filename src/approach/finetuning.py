9c5b538acdb07c4a557bd608c30a210b5c46ebb2
Filip Szatkowski <fmszatkowski@gmail.com>
2023-11-20 11:18:52 +0100

    Fix finetuning arguments

--- src/approach/finetuning.py
+++ src/approach/finetuning.py
@@ -10,12 +10,12 @@ class Appr(Inc_Learning_Appr):
 
     def __init__(self, model, device, nepochs=100, lr=0.05, lr_min=1e-4, lr_factor=3, lr_patience=5, clipgrad=10000,
                  momentum=0, wd=0, multi_softmax=False, wu_nepochs=0, wu_lr=1e-1, wu_fix_bn=False,
-                 wu_scheduler='constant', wu_patience=None, fix_bn=False, eval_on_train=False,
+                 wu_scheduler='constant', wu_patience=None, wu_wd=0., fix_bn=False, eval_on_train=False,
                  select_best_model_by_val_loss=True, logger=None, exemplars_dataset=None, scheduler_milestones=None,
                  all_outputs=False, no_learning=False):
         super(Appr, self).__init__(model, device, nepochs, lr, lr_min, lr_factor, lr_patience, clipgrad, momentum, wd,
-                                   multi_softmax, wu_nepochs, wu_lr, wu_fix_bn, wu_scheduler, wu_patience, fix_bn,
-                                   eval_on_train, select_best_model_by_val_loss, logger, exemplars_dataset,
+                                   multi_softmax, wu_nepochs, wu_lr, wu_fix_bn, wu_scheduler, wu_patience, wu_wd,
+                                   fix_bn, eval_on_train, select_best_model_by_val_loss, logger, exemplars_dataset,
                                    scheduler_milestones, no_learning)
         self.all_out = all_outputs
 
